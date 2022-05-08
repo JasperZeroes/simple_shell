7b975bf2ea0f357e3d5501915adf5ec16c3d0a54
JasperZeroes <jasperobed@gmail.com>
2022-05-08 21:46:11 +0000

    added two files

---
+++ jasper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- prints hello world to standard output
+ * Return: Always 0 (successful)
+ */
+
+
+int main(void)
+{
+	printf("Hello world\n");
+
+	return (0);
+}
